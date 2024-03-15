177f3199946c240c97112a26a478c47859050be4
moin <moinbagban81@gmail.com>
2024-03-15 11:31:37 +0530

    Added login/signup pages

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ def home():
 @app.route("/blogpost")
 def blog():
     return render_template("post-page.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
